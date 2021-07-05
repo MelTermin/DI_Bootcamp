84e71c9818c392daee40d87d0e6e5f122548231e
Melissa Termin <80844457+MelTermin@users.noreply.github.com>
2021-07-05 16:06:44 +0300

    Update script.js

--- Week2/Day2/Daily Challenge/script.js
+++ Week2/Day2/Daily Challenge/script.js
@@ -1,4 +1,14 @@
 let sentence="The movie is not that bad, I like it";
 
-let wordNot= sentence.substring(14,2);
-console.log(wordNot)
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+
+
+if(wordBad > wordNot && wordNot != -1){
+  let a = sentence.substring(wordNot, wordBad + 3);
+  let newSentence = sentence.replace(a, "good");
+  console.log(newSentence);
+
+}else{
+  console.log(sentence)
+}
