346441d12584269f58e85f1fd593648ee5031421
Melissa Termin <80844457+MelTermin@users.noreply.github.com>
2021-07-08 11:51:12 +0300

    Update javascript.js

--- Week2/Day4/Daily Challenge/javascript.js
+++ Week2/Day4/Daily Challenge/javascript.js
@@ -1,9 +1,20 @@
-function frame(str){
-  var border="";
-  for(i=0;i<str.length+4;i++){
-      border += "*"
-  }
-  return border + "\n* "+str+" *\n" + border;
+function starFrame(){
+  let userInput = prompt("Input as many words but separated by commas without any spaces: ");
+  let arrUser = userInput.split(','); 
+
   
-}
-console.log(frame("Hello World in a frame"));
+  let wordLargest = 0;
+  for (item of arrUser){
+      if (item.length>wordLargest){
+          wordLargest = item.length;
+      }
+  }
+
+  
+  console.log("*".repeat(wordLargest+4));
+  for (let i=0; i<arrUser.length;i++){
+      console.log("* " + arrUser[i] + " ".repeat(wordLargest-arrUser[i].length) + " *");
+  }
+  console.log("*".repeat(wordLargest+4));
+  }
+  starFrame();
