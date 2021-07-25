965deea2f656be9149331bc0a1700f3d915564d8
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-07-25 16:01:07 +0300

    xp

---
+++ Week5/Day1/XP_Exercise/javascript.js
@@ -0,0 +1,31 @@
+let marioGame = {
+  detail : "An amazing game!",
+  characters : {
+      mario : {
+        description:"Small and jumpy. Likes princesses.",
+        height: 10,
+        weight: 3,
+        speed: 12,
+      },
+      bowser : {
+        description: "Big and green, Hates princesses.",
+        height: 16,
+        weight: 6,
+        speed: 4,
+      },
+      princessPeach : {
+        description: "Beautiful princess.",
+        height: 12,
+        weight: 2,
+        speed: 2,
+      }
+  },
+}
+
+const json = JSON.stringify(marioGame)
+console.log(json)
+
+//The objects' values now has double quotes
+
+const prettyPrintedJSON = JSON.stringify(marioGame)
+console.log(prettyPrintedJSON)
