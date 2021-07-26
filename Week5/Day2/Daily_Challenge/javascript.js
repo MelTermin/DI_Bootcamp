c0b2a0ce3e8781f8dd3f88c9f10449529f3b935f
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-07-26 17:59:55 +0300

    challenge

---
+++ Week5/Day2/Daily_Challenge/javascript.js
@@ -0,0 +1,55 @@
+const btn=document.getElementById("btn");
+btn.addEventListener("click", displayGif);
+
+const input=document.getElementById("input")
+input.addEventListener("keyup", function() {
+  console.log(input.value)
+});
+
+
+function displayGif(event) {
+  event.preventDefault();
+  let input = document.getElementById("input").value;
+  let form = document.forms[0];
+  form.reset();
+  xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://api.giphy.com/v1/gifs/search?q=${input}&limit=1&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`);
+  xhr.responseType = 'json';
+  xhr.send();
+  xhr.onload = () => {
+    if(xhr.status != 200){
+      console.log(`Error`);
+    }
+    else{
+      loadXml(xhr.response);
+    }
+  }
+}
+
+
+const loadXml = () => {
+  let container = document.querySelector(".gif-container");
+  let source = xhr.response["data"][0]["images"]["downsized"]["url"];
+  let image = document.createElement("img");
+  image.setAttribute("src", source);
+  
+  container.appendChild(image);
+  
+  let deleteButton = document.createElement("button");
+  
+  deleteButton.innerHTML = "Delete gif";
+  container.appendChild(deleteButton);
+  deleteButton.addEventListener("click", () => {
+   container.remove(image);
+   container.remove(deleteButton);
+  });
+}
+
+const removeAllGifs = () => {
+  let container = document.querySelector(".gif-container");
+  container.remove();
+}
+
+
+const deleteAllButton = document.querySelector(".delete");
+deleteAllButton.addEventListener("click", removeAllGifs);
