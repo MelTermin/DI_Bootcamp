fb460dc5973d61f4a901f67ca4e475a4bc4c7563
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-08-01 17:07:12 +0300

    xp-gold

---
+++ Week6/Day1/XP_Gold/javascript.js
@@ -0,0 +1,4 @@
+// --XP Gold
+// -- 	SELECT * FROM students ORDER BY last_name ASC LIMIT 4
+// -- 	SELECT * FROM students ORDER BY birth_date DESC LIMIT 1
+// -- 	SELECT * FROM students where id NOT IN (1,2) ;
