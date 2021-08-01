67eba998dd0123ff25d8287aae79bd2c855dfbae
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-08-01 15:57:24 +0300

    daily

---
+++ Week6/Day1/Daily_Challenge/javascript.js
@@ -0,0 +1,6 @@
+// -- 	SELECT COUNT (*) FROM actors
+	 
+// -- INSERT INTO actors (first_name, last_name, age, number_oscars)
+// -- VALUES ('Britney','','1970-10-08', 5); => even though I added constraints of NOT NULL it added to the table but with a blank row
+
+// select * from actors
