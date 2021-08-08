b845d4e6cf6ceed3d028731db1a5bd8303459aa3
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-08-08 15:17:59 +0300

    XP

---
+++ Week7/Day1/XP/main.js
@@ -0,0 +1,15 @@
+// let a = 5;
+// let b = 10;
+// let sum= a+b
+
+// console.log(`hello you are ${sum}`)
+// console.log("Hi thanks for running with me npm staart")
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+console.log(a+b)
+console.log(x+c)
