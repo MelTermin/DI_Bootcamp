37e3fb8f92276d457ff32809ba83467766244864
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-08-25 15:41:34 +0300

    counter-exercise

---
+++ Week9/Day4/XP/counter-ex/src/redux/actions.js
@@ -0,0 +1,18 @@
+
+export const increment= () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+export const decrement= () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// export const incrementIfOdd= () => {
+//   return {
+//     type: 'INCREMENT_IF_ODD'
+//   }
+// }
